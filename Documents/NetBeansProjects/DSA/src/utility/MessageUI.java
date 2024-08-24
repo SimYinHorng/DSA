9d6258c3da140f28092d7d7a0fa16f624e1d3adf
Sim Yin Horng <simyinhorng27@gmail.com>
2024-08-24 15:20:58 +0800

    utility

--- Documents/NetBeansProjects/DSA/src/utility/MessageUI.java
+++ Documents/NetBeansProjects/DSA/src/utility/MessageUI.java
@@ -9,5 +9,11 @@ package utility;
  * @author user
  */
 public class MessageUI {
-    
+    public static void displayInvalidChoiceMessage() {
+        System.out.println("\nInvalid choice");
+    }
+
+    public static void displayExitMessage() {
+        System.out.println("\nExiting system");
+    }
 }
