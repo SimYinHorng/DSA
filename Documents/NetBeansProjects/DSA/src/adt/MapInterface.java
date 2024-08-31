f6b92b19891fa53bda5d5315efab17f241e00caa
Sim Yin Horng <simyinhorng27@gmail.com>
2024-08-31 18:05:26 +0800

    adjust

--- Documents/NetBeansProjects/DSA/src/adt/MapInterface.java
+++ Documents/NetBeansProjects/DSA/src/adt/MapInterface.java
@@ -1,6 +1,8 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt 
+to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java 
+to edit this template
  */
 package adt;
 
@@ -10,25 +12,25 @@ package adt;
  * @param <K>
  * @param <V>
  */
-public interface MapInterface<K,V> {
-    
-    public V put(K key,V value);
-    
+public interface MapInterface<K, V> {
+
+    public V put(K key, V value);
+
     public V get(K key);
-    
+
     public void remove(K key);
-    
+
     public boolean containsKey(K key);
-    
+
     public boolean containsValue(V values);
-    
+
     public int size();
-    
+
     public void clear();
-    
+
     public ListInterface<V> values();
-    
+
     public SetInterface<K> keySet();
-    
+
     public boolean isEmpty();
 }
